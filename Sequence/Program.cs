f7d22d9e0f39c3b0aebe224592372312c0fc7b3d
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-15 19:39:39 +0300

    Добавьте файлы проекта.

---
+++ Sequence/Program.cs
@@ -0,0 +1,18 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int quantityLevels = 96;
+        int periodOfBossLevel = 7;
+        int numberFirstBossLevel =5;
+
+        for(int i = numberFirstBossLevel; i <= quantityLevels; i+= periodOfBossLevel)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+/*Для этой задачи я решил выбрать цикл for,
+ * Он лучше всего подходит для этой задачи так-как,
+ * Есть четкие границы: начало цикла, шаг цикла и конец цикла.
+ */
